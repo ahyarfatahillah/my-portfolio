86b8f30dde1682fba1e24a7d9662e7996cdf7023
ahyarfatahillah <ahyaredukasi@gmail.com>
2024-08-19 12:02:52 +0800

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Achyar | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Achyar Fatahillah is a full-stack developer with 2 years of experience.",
 };
 
 export default function RootLayout({
